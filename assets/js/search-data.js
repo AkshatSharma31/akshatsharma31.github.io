fc2adfe1080ff8bfe7c93c02b5273ec62be45e31
AkshatSharma31 <154901896+AkshatSharma31@users.noreply.github.com>
2026-05-26 05:33:38 +0000

    Deploying to gh-pages from @ AkshatSharma31/akshatsharma31.github.io@cf094738e95908a03cf5d5292cd7e538051db826 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -496,28 +496,35 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%61%73%32%33%6D%73%30%37%38@%69%69%73%65%72%6B%6F%6C.%61%63.%69%6E", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/AkshatSharma31", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://www.linkedin.com/in/akshat-sharma31", "_blank");
+        },
+      },{
+        id: 'social-orcid',
+        title: 'ORCID',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://orcid.org/0009-0000-8474-5816", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=D2c7uYUAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
