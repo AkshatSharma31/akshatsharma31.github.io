ca636acbd7eb2516ab22f02a180a69f488719228
AkshatSharma31 <154901896+AkshatSharma31@users.noreply.github.com>
2026-05-26 04:59:46 +0000

    Deploying to gh-pages from @ AkshatSharma31/akshatsharma31.github.io@9cd66e25fa377847e640712d4b3ea41a1585fe57 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "AkshatSharma31/akshatsharma.github.io",
+    "data-repo": "AkshatSharma31/akshatsharma31.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
